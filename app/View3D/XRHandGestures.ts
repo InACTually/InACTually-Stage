93c1be5bfb917c67c09464ce57250485ec2afc70
maantho <129731460+maantho@users.noreply.github.com>
2025-08-28 17:10:15 +0200

    Added attribution to XRHandGestures
    Signed-off-by: maantho < 129731460+maantho@users.noreply.github.com>

--- app/View3D/XRHandGestures.ts
+++ app/View3D/XRHandGestures.ts
@@ -1,3 +1,18 @@
+/*
+	InACTually
+	> interactive theater for actual acts
+	> this file is part of the "InACTually Stage", a spatial Interface for orchestrating interactive Media
+
+	Copyright(c) 2025 InACTually Community
+	Licensed under the MIT License.
+	See LICENSE file in the project root for full license information.
+
+	This file is created and substantially modified: 2023-2025
+
+	contributors:
+	Anton Hörig
+*/
+
 import * as THREE from "three";
 
 export default class XRHandGestures {
